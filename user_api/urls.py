b8a5f647e96e8f9c9bee2d50650807bc02f2e4a1
santosh77777 <santoshk43214@gmail.com>
2020-11-09 11:50:41 +0530

    first

--- user_api/urls.py
+++ user_api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.StudentProfileDetail.as_view()),
     path('api/students/', views.StudentProfileDetail.as_view()),
-    path('api/student/<int:pk>/', views.StudentProfileDetail.as_view()),
+    path('api/student/<int:id>/', views.StudentProfileDetail.as_view()),
 ]
